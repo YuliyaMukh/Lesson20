37488be98675fe8efcd91f8a4dba3eb0b9169ddc
ProxyVT <86965169+ProxyVT@users.noreply.github.com>
2022-11-08 21:56:56 +0300

    module

---
+++ some_module.py
@@ -0,0 +1,16 @@
+__all__ = ["A", "test_a"]
+
+a = 10
+b = 20
+
+def test_a():
+    print("test_a from module some1")
+
+def test_b():
+    print("test_b")
+
+
+class A:
+    pass
+class B:
+    pass
---
+++ test_module.py
@@ -0,0 +1,16 @@
+# 1)
+# import some_module
+
+
+# 2)
+# from some_module import  __a, __b, A, B
+#
+# a = A()
+
+# from some_module import __a as alfa
+# a = alfa
+
+
+3)
+from some_module import  *
+a = test_a()
